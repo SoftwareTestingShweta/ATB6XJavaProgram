27b8e30e276cae6b05c1c58e9cd873a08ed90789
SoftwareTestingShweta <shweta.telka@gmail.com>
2024-04-12 11:04:08 +0530

    12 04 2024

---
+++ src/ex_11042024/FirstClass.java
@@ -0,0 +1,15 @@
+package ex_11042024;
+
+public class FirstClass {
+    public static void main(String[] args) {
+
+        System.out.println("My name is shweta");
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(2-2);
+        System.out.println(5986+544);
+        System.out.println(5986+540000);
+
+    }
+}
---
+++ src/ex_11042024/Lab001.java
@@ -0,0 +1,24 @@
+package ex_11042024;
+
+public class Lab001 {
+
+    public static void main(String[] args) {
+
+        int cakeprice = 400;
+
+        if (cakeprice < 500)
+
+        {
+            System.out.println("buy the cake");
+            System.out.println("Value of cake is : "+  cakeprice);
+
+        }
+        else
+        {
+            System.out.println("Dont buy the cake");
+        }
+
+    }
+
+}
+
