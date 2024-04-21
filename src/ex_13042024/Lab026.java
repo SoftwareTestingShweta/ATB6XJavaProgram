0dd814934bd98391d6357b8bba707b164624edbd
SoftwareTestingShweta <shweta.telka@gmail.com>
2024-04-22 01:17:39 +0530

    New code 18-04 added

--- src/ex_13042024/Lab026.java
+++ src/ex_13042024/Lab026.java
@@ -5,6 +5,18 @@ public class Lab026 {
     public static void main(String[] args) {
         byte a = 10;
         byte b = 18;
+
+         if(a != b)
+         {
+             System.out.println("it is true");
+         }
         System.out.println(a+b);
+
+         int x = 10;
+         int y = x++;
+
+        System.out.println(x);
+        System.out.println(y);
+
     }
 }
