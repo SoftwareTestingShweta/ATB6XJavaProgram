35244f133fa6c1a403566f622fecf0b03d58f9e6
SoftwareTestingShweta <shweta.telka@gmail.com>
2024-04-25 17:37:55 +0530

    New code 25-04 added

---
+++ src/ex_25042024/Lab083.java
@@ -0,0 +1,15 @@
+package ex_25042024;
+
+public class Lab083 {
+
+    public static void main(String[] args) {
+        int a=100;
+        switch(a){
+            case 100:
+                System.out.println("Val - 100");
+//            case 98:
+//                System.out.println("Val - 98");
+        }
+    }
+
+}
